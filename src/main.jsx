48c0af5441df4896eed9609362b86d17adb99db4
devsamuelsilva <devsamuelsilva@gmail.com>
2024-07-02 18:43:49 -0300

    Criando pagina de LOGIN com MUI

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 //import './index.css'
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+import {createBrowserRouter, RouterProvider,} from "react-router-dom";
+import Login from './components/pages/Login.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login/>,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    {/* <App /> */}
   </React.StrictMode>,
 )
